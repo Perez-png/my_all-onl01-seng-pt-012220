7c02ea671808cf48ce81c5402692e98d218b8a16
Xavier Perez <xperez007@gmail.com>
2020-01-28 16:31:06 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,6 +8,9 @@ yield(collection[i])
       end
   end
   
+  if
+
+  
 my_all?([1,2,3]) {|i| i < 2}
   block_return_values = [true,
   false, false]
@@ -16,7 +19,21 @@ my_all?([1,2,3]) {|i| i < 2}
   require 'pry'
 
 def my_all?(collection)
+  i = 0 
+  block_return_values = []
+  while i < collection.length
+    block_return_values <<
+yield(collection{[i]})
+  i = i + 1  
+end
 
+  if
+block_return_values.include?
+(false)
+  false
+else
+  true
+end
 end
 
 def my_all?([1,2,3])
