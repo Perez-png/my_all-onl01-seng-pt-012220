fc6be6518a94b24cc020811f54b312f44d0f7925
Xavier Perez <xperez007@gmail.com>
2020-01-28 16:21:17 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,10 +4,18 @@ def my_all?(collection)
     i = i + 1 
       end
   end
-  my_all?([1,2,3]) {|i| i < 2}
+my_all?([1,2,3]) {|i| i < 2}
   
   require 'pry'
 
 def my_all?(collection)
 
+end
+
+def my_all?([1,2,3])
+  i = 0 
+  while i < 3
+    yield(1)
+    i = 0 + 1 
+  end
 end
